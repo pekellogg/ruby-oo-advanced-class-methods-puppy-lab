802be70a0513a6188da61e7a2e0438be24198a5a
Peyton <pekellogg@hotmail.com>
2021-09-29 14:30:51 -0700

    commit tree 566a7615444211f88adb8f261e3e93eabe078cc0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+
+    attr_reader :name
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each { |dog| puts dog.name }
+    end
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+end
